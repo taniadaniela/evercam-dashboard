3831323b15ad9bc6e4874de9400db6d52bfb8be2
Peter Wood <pwood@blacknorth.com>
2014-02-27 14:47:18 +0000

    First check in for the Evercam dashboard Rails application.

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
+
+module EvercamDashboard
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Incorporate Bootstrap elements.
+    config.assets.paths << "#{Rails.root.to_s}/vendor/assets/fonts"
+    config.assets.precompile += ['bootstrap-3.1.0.min.css',
+                                 'bootstrap-theme-3.1.0.min.css',
+                                 'bootstrap-3.1.0.min.js',
+                                 'glyphicons-halflings-regular.eot',
+                                 'glyphicons-halflings-regular.svg',
+                                 'glyphicons-halflings-regular.ttf',
+                                 'glyphicons-halflings-regular.woff']
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EvercamDashboard::Application.config.secret_key_base = 'fd655af7a44f28d11ff47d58320b313af17002c4e5c0fa0ae1c893e0e6a15c86495736d4fae806b94d879764a9e6e0296e280118c1f39a450246ed7eca99ae0c'

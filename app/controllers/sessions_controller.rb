e6de0ba74eebe25e2a49813463e619be4ad57ef9
tjama <tomasz.jama@mhlabs.net>
2014-03-24 12:51:00 +0000

    allow username to be used for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = Default::User.find_by(email: params[:session][:email].downcase)
+    @user = Default::User.find_by(email: params[:session][:email].downcase) || Default::User.find_by(username: params[:session][:email].downcase)
     if !@user.nil? and @user.password_bcrypt == params[:session][:password]
       sign_in @user
       redirect_to :cameras_index

dc4c834f974de7bbce810e1e50f302c62817b210
Tomasz Jama <gvs666@gmail.com>
2014-04-09 16:23:17 +0100

    cookies will work on subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-EvercamDashboard::Application.config.session_store :cookie_store, key: '_evercam_dashboard_session'
+EvercamDashboard::Application.config.session_store :cookie_store, key: '_evercam_dashboard_session', domain: '.evercam.io'

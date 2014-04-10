6ed82709a2de95642c4f82fa603bc5751793bead
Peter Wood <pwood@blacknorth.com>
2014-04-10 11:30:55 +0100

    Added tests for the sharing controller update_camera method.

--- spec/controllers/sharing_controller_spec.rb
+++ spec/controllers/sharing_controller_spec.rb
@@ -6,6 +6,10 @@ describe SharingController do
          create(:private_camera)
       }
 
+      let!(:other_camera) {
+         create(:private_camera)
+      }
+
       let(:owner) {
          camera.owner
       }
@@ -14,14 +18,49 @@ describe SharingController do
          {api_id: owner.api_id, api_key: owner.api_key}
       }
 
+      let(:parameters) {
+         {:discoverable => false,
+          :id =>           camera.exid,
+          :public =>       false}
+      }
+
       it "returns success for a valid request" do
-         #stub_request(:post, "https://api.evercam.io/v1/users").to_return(:status => 200, :body => "", :headers => {})
-         parameters = {public: false, discoverable: false}.merge(credentials)
-         post :update_camera, parameters
+         stub_request(:patch, "https://api.evercam.io/v1//cameras/#{camera.exid}?api_id=#{owner.api_id}&api_key=#{owner.api_key}").
+            to_return(:status => 200, :body => "", :headers => {})
+
+         post :update_camera, parameters.merge(credentials), {user: owner.email}
          expect(response.status).to eq(200)
          output = JSON.parse(response.body)
          expect(output.include?("success")).to eq(true)
          expect(output["success"]).to eq(true)
+      end
+
+      it "returns failure for an non-existent camera" do
+         stub_request(:patch, "https://api.evercam.io/v1//cameras/blah?api_id=#{owner.api_id}&api_key=#{owner.api_key}").
+            to_return(:status => 404, :body => "", :headers => {})
+
+         parameters[:id]  = "blah"
+         post :update_camera, parameters.merge(credentials), {user: owner.email}
+         expect(response.status).to eq(200)
+         output = JSON.parse(response.body)
+         expect(output.include?("success")).to eq(true)
+         expect(output.include?("message")).to eq(true)
+         expect(output["success"]).to eq(false)
+         expect(output["message"]).to eq("Failed to update camera permissions.")
+      end
+
+      it "returns failure for a camera that is owned by someone else" do
+         stub_request(:patch, "https://api.evercam.io/v1//cameras/#{other_camera.exid}?api_id=#{owner.api_id}&api_key=#{owner.api_key}").
+            to_return(:status => 403, :body => "", :headers => {})
+
+         parameters[:id]  = other_camera.exid
+         post :update_camera, parameters.merge(credentials), {user: owner.email}
+         expect(response.status).to eq(200)
+         output = JSON.parse(response.body)
+         expect(output.include?("success")).to eq(true)
+         expect(output.include?("message")).to eq(true)
+         expect(output["success"]).to eq(false)
+         expect(output["message"]).to eq("Failed to update camera permissions.")
       end
    end
 end

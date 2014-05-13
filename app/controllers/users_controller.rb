0231bd00fe35282a5d292b7f4ea215a04e204169
Tomasz Jama <gvs666@gmail.com>
2014-05-13 13:47:53 +0100

    qucik fix for empty token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -140,7 +140,7 @@ class UsersController < ApplicationController
     if user.nil? or token != user.reset_token or user.token_expires_at < Time.now
       flash[:message] = 'Invalid username or token'
     else
-      user.update(password: params[:password], reset_token: '', token_expires_at: Time.now)
+      user.update(password: params[:password], reset_token: '', token_expires_at: Time.now - 1.hour)
       sign_in user
       redirect_to "/", message: 'Your password has been changed'
     end

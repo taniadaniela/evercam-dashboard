6c87e98e02a1c062d2db497e3431636d3638ccec
Peter Wood <pwood@blacknorth.com>
2014-05-16 16:04:06 +0100

    Added some exception types to be ignored by Airbrake.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
 Airbrake.configure do |config|
   config.api_key = '61fea9c916a4f43f5f2c2e882b1f2983'
+  config.ignore << "Evercam::CameraOfflineError"
+  config.ignore << "Evercam::NotFoundError"
 end

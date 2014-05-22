837a26cbb5f7fcb10193a3bb5641ee67ad223b94
Tomasz Jama <gvs666@gmail.com>
2014-05-22 12:26:38 +0100

    cleaned up parameters

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -106,7 +106,10 @@ class CamerasController < ApplicationController
       @camera = output['cameras'][0]
       @page = (params[:page].to_i - 1) || 0
       @types = ['created', 'accessed', 'viewed', 'edited', 'captured', 'shared', 'stopped sharing', 'online', 'offline']
-      response = API_call("cameras/#{params[:id]}/logs", :get, objects: 'true', page: @page, types: params[:types], from: params[:from], to: params[:to])
+      log_params = { objects: 'true', page: @page, types: params[:types]}
+      log_params[:from] = params[:from] unless params[:from].blank?
+      log_params[:to] = params[:to] unless params[:to].blank?
+      response = API_call("cameras/#{params[:id]}/logs", :get, log_params)
       @logs = JSON.parse(response.body)['logs']
       @pages = JSON.parse(response.body)['pages']
       @share   = nil

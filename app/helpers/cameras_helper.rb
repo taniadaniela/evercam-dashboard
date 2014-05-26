06d2d59ddf653e0b94b1544dff896838b98a2aaf
Peter Wood <pwood@blacknorth.com>
2014-05-26 11:13:51 +0100

    Corrections for the wrapper work.

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -1,4 +1,21 @@
 module CamerasHelper
+  def get_evercam_api
+    configuration = Rails.application.config
+    parameters    = {logger: Rails.logger}
+    if current_user
+      parameters = parameters.merge(api_id: current_user.api_id,
+                                    api_key: current_user.api_key)
+    end
+    settings      = {}
+    begin
+      settings = (configuration.evercam_api || {})
+    rescue => error
+      # Deliberately ignored.
+    end
+    parameters    = parameters.merge(settings) if !settings.empty?
+    Evercam::API.new(parameters)
+  end
+
   def preview(camera, live=false)
     begin
       res       = get_evercam_api.get_latest_snapshot(camera['id'], true)

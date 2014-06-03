703fea920f4d2542ff28f4ffdb1419da89b73a56
Tomasz Jama <gvs666@gmail.com>
2014-06-03 17:18:03 +0100

    most of the pages are for logged in users only

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_filter :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:swagger]
   include SessionsHelper
   include ApplicationHelper
   layout "forswagger", only: [:swagger]

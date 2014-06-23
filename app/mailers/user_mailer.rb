8b30a9ace2db1c692a5a4eea387617eb4ede46fc
Tomasz Jama <gvs666@gmail.com>
2014-06-23 15:49:47 +0100

    testing inline emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,11 @@ class UserMailer < ActionMailer::Base
       @camera_id = camera_id
       @user      = user
       @key       = key
-      @snapshot  = snapshot
+      @add_snap = false
+      unless snapshot.nil?
+        attachments.inline['snapshot.jpg'] = snapshot
+        @add_snap = true
+      end
       mail(to: email, subject: "#{user.username} has shared a camera with you")
    end
 

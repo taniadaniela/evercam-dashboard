4dd7e626af3512a51d1b455b485a37deb843d219
Tomasz Jama <gvs666@gmail.com>
2014-07-28 14:01:54 +0100

    GC profiling for new relic

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module EvercamDashboard
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    GC::Profiler.enable
 
     # Incorporate Bootstrap elements.
     config.assets.paths << "#{Rails.root.to_s}/vendor/assets/fonts"

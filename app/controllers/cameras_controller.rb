d739f2b57449287baf7a81ad96f33d57fd74c0b3
Tomasz Jama <gvs666@gmail.com>
2014-08-11 11:58:53 +0100

    owner email

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -149,6 +149,9 @@ class CamerasController < ApplicationController
       if @camera['owner'] != current_user.username
         @share = api.get_camera_share(params[:id], current_user.username)
         redirect_to action: 'index' if @share.nil?
+        @owner = User.where(:username => @camera['owner'])
+      else
+        @owner = current_user
       end
       @camera_shares = Rails.cache.fetch("#{current_user.username}/#{params[:id]}/cam_shares", expires_in: 5.minutes) do
         api.get_camera_shares(params[:id])

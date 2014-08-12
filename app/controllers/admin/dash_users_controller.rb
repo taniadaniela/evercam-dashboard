1839cd4ea1353795282645d478d34597743504f6
Milos Mosic <mosic.milos@gmail.com>
2014-08-12 13:57:54 +0000

    Added number of cameras and confirmed email to admin

--- app/controllers/admin/dash_users_controller.rb
+++ app/controllers/admin/dash_users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashUsersController < AdminController
 
   def index
-    @dash_users = DashUser.all.includes(:dash_country)
+    @dash_users = DashUser.all.includes(:dash_country, :dash_cameras)
   end
 
   def show

c5f5d98f5feacac5e3a1b10e81f9e13d0d0d0b34
Milos Mosic <mosic.milos@gmail.com>
2014-09-30 16:04:26 +0200

    Added ability to sign in as other users from admin dashboard

--- app/controllers/admin/dash_users_controller.rb
+++ app/controllers/admin/dash_users_controller.rb
@@ -8,4 +8,16 @@ class Admin::DashUsersController < AdminController
     @dash_user = DashUser.find(params[:id])
   end
 
+  def impersonate
+    user = User[params[:id]]
+    if user
+      sign_out
+      sign_in user
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  rescue ActionController::RedirectBackError
+    redirect_to admin_path
+  end
 end

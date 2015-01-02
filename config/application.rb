4084ff5e87812a90093c51f0a42728aab4dcb433
Miloš Mošić <mosic.milos@gmail.com>
2015-01-02 01:49:15 +0000

    Explicitly required evercam gem

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "active_record/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require "evercam"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

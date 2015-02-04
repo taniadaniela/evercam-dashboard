9cf6a2acb09861ef755a2a1444ef7ec66a0997cb
Azhar Malik <azharmalik02@gmail.com>
2015-02-04 15:35:01 +0500

    Added missing parameters in api url

--- spec/controllers/cameras_controller_spec.rb
+++ spec/controllers/cameras_controller_spec.rb
@@ -218,7 +218,7 @@ describe CamerasController do
           to_return(:status => 200, :body => '{"share_requests": []}', :headers => {})
         stub_request(:get, "#{EVERCAM_API}cameras/#{params['camera-id']}/shares/requests.json?api_id=#{camera.owner.api_id}&api_key=#{camera.owner.api_key}").
            to_return(:status => 200, :body => '{"shares": []}', :headers => {})
-        stub_request(:get, "#{EVERCAM_API}cameras/#{params['camera-id']}/webhooks.json?api_id=#{camera.owner.api_id}&api_key=#{camera.owner.api_key}").
+        stub_request(:get, "#{EVERCAM_API}cameras/#{params['camera-id']}/webhooks.json?api_id=#{camera.owner.api_id}&api_key=#{camera.owner.api_key}&camera_id=#{params['camera-id']}").
            to_return(:status => 200, :body => '{"webhooks": [{}]}', :headers => {})
 
         session['user'] = camera.owner.email
@@ -232,7 +232,7 @@ describe CamerasController do
       it "redirects to cameras index" do
         stub_request(:get, "#{EVERCAM_API}cameras/#{camera2.exid}.json?api_id=#{user.api_id}&api_key=#{user.api_key}&thumbnail=true").
           to_return(status: 200, headers: {}, body: "{\"cameras\": [{}]}")
-        stub_request(:get, "#{EVERCAM_API}cameras/#{camera2.exid}/shares.json?api_id=#{user.api_id}&api_key=#{user.api_key}").
+        stub_request(:get, "#{EVERCAM_API}cameras/#{camera2.exid}/shares.json?api_id=#{user.api_id}&api_key=#{user.api_key}&user_id=#{user.api_id}").
           to_return(:status => 200, :body => "{\"shares\": []}", :headers => {})
         stub_request(:get, "#{EVERCAM_API}cameras.json?api_id=#{user.api_id}&api_key=#{user.api_key}&include_shared=true&thumbnail=true&user_id=#{user.username}").
           to_return(:status => 200, :body => '{"cameras": []}', :headers => {})

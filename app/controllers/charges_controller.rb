ec07739f1e59ac9bb3db0ca1cc1ccb9dd6c600c3
Ciaran Hanrahan <ciaran@evercam.io>
2015-02-09 13:16:41 +0000

    subscribe to plans

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,42 +1,46 @@
-class ChargesController < ApplicationController 
-  before_filter :authenticate_user! 
-  include SessionsHelper 
-  include ApplicationHelper  
+class ChargesController < ApplicationController
+  before_filter :authenticate_user!
+  include SessionsHelper
+  include ApplicationHelper
 
-  def new 
-    @cameras = load_user_cameras(true, true) 
-  end  
-
-  def create 
+  def new
     @cameras = load_user_cameras(true, true)
-      
-    @email = current_user.email 
-    @amount = params[:amount]  
-
-    token = params[:token]  
-
-  if token.blank?  
-
-    customer = Stripe::Customer.create( 
-      :email => @email, 
-      :card  => params[:stripeToken] 
-    )  
-
-  current_user.billing_id = customer.id 
-  current_user.save  
-
-  else  
-
-    customer = Stripe::Customer.retrieve(token) 
-    customer.subscriptions.create( 
-      :plan => "silver-monthly" 
-    )     
-
-  end
-      
-  rescue Stripe::CardError => e 
-    flash[:error] = e.message 
-    redirect_to charges_path 
   end
 
-  end  
+  def create
+
+    @cameras = load_user_cameras(true, true)
+
+    @email = current_user.email
+    @amount = params[:amount]
+    @plan = params[:plan]
+
+    token = params[:token]
+
+    if token.blank?
+
+      customer = Stripe::Customer.create(
+        :email => @email,
+        :card  => params[:stripeToken],
+        :plan => @plan
+      )
+
+      current_user.billing_id = customer.id
+      current_user.save
+
+    else
+
+      customer = Stripe::Customer.retrieve(token)
+      customer.subscriptions.create(
+       :plan => @plan
+      )
+
+    end
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
+
+end
+

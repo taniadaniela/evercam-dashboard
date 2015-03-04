bd12ee9e387143e5cea2b2f35e4f9f3a3724d995
Miloš Mošić <mosic.milos@gmail.com>
2015-03-04 18:55:33 +0100

    Fixed stripe controller code

--- app/helpers/stripe_customers_helper.rb
+++ app/helpers/stripe_customers_helper.rb
@@ -32,12 +32,6 @@ module StripeCustomersHelper
     else
       false
     end
-<<<<<<< Updated upstream
-    stripe_customer = Stripe::Customer.retrieve(current_user.billing_id)
-    stripe_customer.default_source.present?
-=======
-    user_account_error
->>>>>>> Stashed changes
   end
 
   def stripe_customer_without_current_cards?

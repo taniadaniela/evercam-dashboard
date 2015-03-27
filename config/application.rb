17de622bb35007c7ab613ba040b4b0735327aae4
Azhar Malik <azharmalik02@gmail.com>
2015-03-27 13:00:01 +0500

    Revert image changes and added .svg in precompile list

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module EvercamDashboard
     config.assets.paths << "#{Rails.root.to_s}/vendor/assets/javascripts"
     config.assets.paths << "#{Rails.root.to_s}/lib/assets/javascripts"
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.svg]
     config.assets.precompile += %w[
       snapshot_navigator_widget.js
       admin/admin.js

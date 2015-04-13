a1a726a43bce8f3093f6e164ebbee3f16f945823
Azhar Malik <azharmalik02@gmail.com>
2015-04-13 12:46:38 +0500

    Remove intercom entirely

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
   skip_before_action :authenticate_user!, only: [:swagger]
-  skip_after_filter :intercom_rails_auto_include, only: [:live]
   include SessionsHelper
   include ApplicationHelper
 

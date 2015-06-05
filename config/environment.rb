1b8f041e54348e3ff4b83368c77d512989b296fb
Miloš Mošić <mosic.milos@gmail.com>
2015-06-05 21:49:06 +0200

    Disabled duplicate logs

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-Rails.logger = Logger.new(STDOUT)
-
 # Initialize the Rails application.
 EvercamDashboard::Application.initialize!
 Mime::Type.register "application/pdf", :pdf

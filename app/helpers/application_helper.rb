acad850338b39e9e76302e2b3abba03e96a08a4e
Miloš Mošić <mosic.milos@gmail.com>
2015-10-21 07:21:00 +0200

    Fix mixed content warning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
   def avatar_url
     gravatar_id = Digest::MD5.hexdigest(current_user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
+    "//gravatar.com/avatar/#{gravatar_id}.png"
   end
 
   def vendors

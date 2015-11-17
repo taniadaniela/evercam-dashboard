56eace9f5d3ed364d6a8f48ca02553830fead7c7
Miloš Mošić <mosic.milos@gmail.com>
2015-11-17 21:03:10 +0100

    Fix constants in config

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,11 +1,4 @@
 TIMEOUT = 5
-begin
-  EVERCAM_API       = evercam_api_url
-  EVERCAM_MEDIA_API = evercam_media_api_url
-rescue
-  EVERCAM_API       = 'https://api.evercam.io/v1/'
-  EVERCAM_MEDIA_API = 'https://media.evercam.io/v1/'
-end
 
 def evercam_api_url
   settings    = EvercamDashboard::Application.config.evercam_api
@@ -21,4 +14,12 @@ def evercam_media_api_url
   host        = (settings[:host] || "media.evercam.io")
   host        = "#{host}:#{settings[:port]}" if settings.include?(:port)
   "#{scheme}://#{host}/v1/"
+end
+
+begin
+  EVERCAM_API       = evercam_api_url
+  EVERCAM_MEDIA_API = evercam_media_api_url
+rescue
+  EVERCAM_API       = 'https://api.evercam.io/v1/'
+  EVERCAM_MEDIA_API = 'https://media.evercam.io/v1/'
 end

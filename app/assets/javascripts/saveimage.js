7f5cefd572fb8b1cefcd7b1ff607472b4b2d9d38
Muneeb Rana <muneeb@evercam.io>
2015-12-16 12:26:48 +0500

    indentations removed

--- app/assets/javascripts/saveimage.js
+++ app/assets/javascripts/saveimage.js
@@ -2,21 +2,19 @@
  * Created by evercam on 4/6/15.
  */
 var SaveImage = (function() {
-    "use strict";
+  "use strict";
 
-    var that = {};
+   var that = {};
 
-    that.init = function() {
+   that.init = function() {
 
-    };
+   };
 
 
     function getMobileOperatingSystem() {
         var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
         if( userAgent.match( /Android/i ) )
         {
-
             return 'Android';
         }
         else
@@ -26,29 +24,25 @@ var SaveImage = (function() {
     }
 
     that.save = function (fileURL, fileName) {
-
-            // for non-IE
-            if (!window.ActiveXObject) {
-                var save = document.createElement('a');
-                save.href = fileURL;
-                save.target = '_blank';
-                if(getMobileOperatingSystem() == "unknown") {
-                    save.download = fileName || 'unknown';
-                }
-
-                var event = document.createEvent('Event');
-                event.initEvent('click', true, true);
-                save.dispatchEvent(event);
-                (window.URL || window.webkitURL).revokeObjectURL(save.href);
+        // for non-IE
+        if (!window.ActiveXObject) {
+            var save = document.createElement('a');
+            save.href = fileURL;
+            save.target = '_blank';
+            if(getMobileOperatingSystem() == "unknown") {
+                save.download = fileName || 'unknown';
             }
-
-            // for IE
-            else if (!!window.ActiveXObject && document.execCommand) {
-                var _window = window.open(fileURL, '_blank');
-                _window.document.close();
-                _window.document.execCommand('SaveAs', true, fileName || fileURL)
-                _window.close();
-            }
+            var event = document.createEvent('Event');
+            event.initEvent('click', true, true);
+            save.dispatchEvent(event);
+            (window.URL || window.webkitURL).revokeObjectURL(save.href);
+        }
+        // for IE
+        else if (!!window.ActiveXObject && document.execCommand) {
+            var _window = window.open(fileURL, '_blank');
+             _window.document.close();
+             _window.document.execCommand('SaveAs', true, fileName || fileURL)
+             _window.close();
         }
     };
 

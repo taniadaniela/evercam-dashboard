7f11f0d53a71fc10fb3894be65884d0e7aef62c4
Muneeb Rana <muneeb@evercam.io>
2016-01-26 14:04:53 +0500

    hound error fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -76,7 +76,7 @@ describe UsersController do
     it "signs in and redirects to cameras index" do
       pending
       stub_request(:post, "#{EVERCAM_API}users.json").
-        with(:body => { "email" => "#{CGI.escape(new_user_params[:user][:email])}", "firstname" => "Joe", "lastname" => "Bloggs", "password" => "password", "username" => "#{new_user_params[:user][:username]}", "last_login_at" => "#{ new_user_params[:user][:last_login_at] }" } ).
+        with(:body => { "email" => "#{CGI.escape(new_user_params[:user][:email])}", "firstname" => "Joe", "lastname" => "Bloggs", "password" => "password", "username" => "#{new_user_params[:user][:username]}", "last_login_at" => "#{new_user_params[:user][:last_login_at]}" }).
         to_return(:status => 200, :body => '{"users": [{}]}', :headers => {})
 
       stub_request(:post, "#{EVERCAM_API}users.json").

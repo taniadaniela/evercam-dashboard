72d7e13eb69accf57137acf783ef902d84b32029
Muneeb Rana <muneeb@evercam.io>
2016-02-02 11:08:35 +0500

    schema updated

--- db/structure.sql
+++ db/structure.sql
@@ -508,7 +508,7 @@ CREATE TABLE users (
     api_id text,
     api_key text,
     is_admin boolean DEFAULT false NOT NULL,
-    stripe_customer_id text
+    stripe_customer_id text,
     last_login_at timestamp with time zone
 );
 

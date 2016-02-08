108cd86e0d20884e6da234f639b17ac530ecf77e
Muneeb Rana <muneeb@evercam.io>
2016-02-08 15:45:44 +0500

    cloud recording widget fix

--- app/assets/javascripts/snapshot_navigator_widget.js
+++ app/assets/javascripts/snapshot_navigator_widget.js
@@ -9,6 +9,27 @@
 //= require screenfull
 //= require saveimage.js
 //= require evercam.js
+//= require fullcalendar/fullcalendar.js
 //= require cameras/single/snapshots_navigator.js
 
+//=
+//= require jquery.uniform/jquery.uniform.js
+//= require datetimepicker/jquery.datetimepicker.js
+//= require iCheck
+
+//=
+//= require alerts
+//= require cameras/single.js
+//= require bootstrap-toggle
+//=
+
+//= require jquery.browser
+//= require lib/shred.bundle
+//= require lib/jquery.slideto.min
+//= require lib/jquery.wiggle.min
+//= require lib/jquery.ba-bbq.min
+//= require lib/handlebars-1.0.0
+//= require lib/underscore-min
+//= require lib/backbone-min
+//= require lib/highlight.7.3.pack
 

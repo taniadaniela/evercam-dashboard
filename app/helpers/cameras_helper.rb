eafd7d25bd14b7308e8e4ece52b4d7f369e1f01e
Miloš Mošić <mosic.milos@gmail.com>
2016-02-19 20:37:36 +0100

    Switch to using new thumbnail endpoint instead of thumbnail_url

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -1,17 +1,6 @@
 module CamerasHelper
   def preview(camera, refresh=false)
-    thumbnail_url = camera['thumbnail_url']
-    proxy = "#{EVERCAM_API}cameras/#{camera['id']}/recordings/snapshots/latest/jpg?api_id=#{current_user.api_id}&api_key=#{current_user.api_key}"
-
-    # TODO: replace with image_tag
-    if thumbnail_url.blank? && !camera['is_online']
-      return "<img id='message' class='no-thumbnail' src='#{asset_path("offline.png")}' data-proxy=#{proxy}>".html_safe
-    elsif thumbnail_url.blank? && camera['is_online']
-      return "<img class='snapshot-proxy' src='#{proxy}' data-proxy=#{proxy}>".html_safe
-    elsif refresh
-      return "<img id='message' class='snapshot-proxy' src='#{thumbnail_url}' data-proxy=#{proxy}'>".html_safe
-    else
-      return "<img class='snapshot-proxy' src='#{thumbnail_url}' data-proxy=#{proxy}>".html_safe
-    end
+    thumbnail_url = "#{EVERCAM_MEDIA_API}cameras/#{camera['id']}/thumbnail?api_id=#{current_user.api_id}&api_key=#{current_user.api_key}"
+    image_tag(thumbnail_url, alt: "camera thumbnail")
   end
 end

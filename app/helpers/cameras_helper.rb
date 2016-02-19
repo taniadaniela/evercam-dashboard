a40e07a3b66534a0f779d678fff121a5665ef693
Miloš Mošić <mosic.milos@gmail.com>
2016-02-19 20:45:17 +0100

    Rename unused variable

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -1,5 +1,5 @@
 module CamerasHelper
-  def preview(camera, refresh=false)
+  def preview(camera, _refresh = false)
     thumbnail_url = "#{EVERCAM_MEDIA_API}cameras/#{camera['id']}/thumbnail?api_id=#{current_user.api_id}&api_key=#{current_user.api_key}"
     image_tag(thumbnail_url, alt: "camera thumbnail")
   end

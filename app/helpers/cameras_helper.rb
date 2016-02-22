227ab8a3d7689b61ae816b8c47501f14243b1dc8
Miloš Mošić <mosic.milos@gmail.com>
2016-02-22 13:34:29 +0100

    Extract thumbnail_url to a method

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -1,6 +1,9 @@
 module CamerasHelper
   def preview(camera, _refresh = false)
-    thumbnail_url = "#{EVERCAM_MEDIA_API}cameras/#{camera['id']}/thumbnail?api_id=#{current_user.api_id}&api_key=#{current_user.api_key}"
-    image_tag(thumbnail_url, alt: "camera thumbnail")
+    image_tag(thumbnail_url(camera), alt: "camera thumbnail")
+  end
+
+  def thumbnail_url(camera)
+    "#{EVERCAM_MEDIA_API}cameras/#{camera['id']}/thumbnail?api_id=#{current_user.api_id}&api_key=#{current_user.api_key}"
   end
 end

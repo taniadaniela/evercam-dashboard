67211b42209465eef6152b4dd0389132d1cc240e
affan00 <affan@evercam.io>
2016-05-19 18:05:29 +0500

    fix hound errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -97,7 +97,7 @@ class UsersController < ApplicationController
   def user_account
     @cameras = load_user_cameras(true, false)
     @countries = Country.all
-    @countries.sort_by!{ |e| e.name.downcase }
+    @countries.sort_by! { |e| e.name.downcase }
     render layout: "user-account"
   end
 

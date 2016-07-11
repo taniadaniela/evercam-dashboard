120707424512d219ac75e86ee520c9dbb203111a
Miloš Mošić <mosic.milos@gmail.com>
2016-07-12 01:40:53 +0200

    Use evercam-media endpoints for AJAX requests

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,13 +1,5 @@
 TIMEOUT = 5
 
-def evercam_api_url
-  settings    = EvercamDashboard::Application.config.evercam_api
-  scheme      = (settings[:scheme] || "https")
-  host        = (settings[:host] || "api.evercam.io")
-  host        = "#{host}:#{settings[:port]}" if settings.include?(:port)
-  "#{scheme}://#{host}/v1/"
-end
-
 def evercam_media_api_url
   settings    = EvercamDashboard::Application.config.evercam_media_api
   scheme      = (settings[:scheme] || "https")
@@ -17,9 +9,9 @@ def evercam_media_api_url
 end
 
 begin
-  EVERCAM_API       = evercam_api_url
+  EVERCAM_API       = evercam_media_api_url
   EVERCAM_MEDIA_API = evercam_media_api_url
 rescue
-  EVERCAM_API       = 'https://api.evercam.io/v1/'
+  EVERCAM_API       = 'https://media.evercam.io/v1/'
   EVERCAM_MEDIA_API = 'https://media.evercam.io/v1/'
 end

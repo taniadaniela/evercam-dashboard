8daf82a8785be9ab1446f2f2389e88c45a6e0877
azharmalik3 <azharmalik02@gmail.com>
2017-02-08 16:17:42 +0500

    Removed vendor models direct db calls code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,18 +6,6 @@ module ApplicationHelper
     "//gravatar.com/avatar/#{gravatar_id}"
   end
 
-  def vendors
-    Vendor.order(:name).all
-  end
-
-  def models(vendor_id)
-    VendorModel
-      .select(:exid, :name, :jpg_url, :mjpg_url, :h264_url, :vendor_id, :config)
-      .where(vendor_id: vendor_id)
-      .order(Sequel.lit("case when name = 'Default' then 0 else 1 end, name"))
-      .all
-  end
-
   def timezones
     Timezone::Zone.names.to_a
   end

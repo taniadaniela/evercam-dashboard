43f212fb122c43892e47bde77153c25d9f89f5f4
Azhar Malik <azharmalik02@gmail.com>
2018-08-08 13:16:13 +0500

    removed config from models

--- app/models/evercam_models/vendor_model.rb
+++ app/models/evercam_models/vendor_model.rb
@@ -2,14 +2,5 @@ class VendorModel < Sequel::Model
   many_to_one :vendor
   one_to_many :cameras
   DEFAULT = 'Default'
-  # Returns a deep merge of any config values set for this
-  # model with the default vendor config
-  def config
-    if DEFAULT != name
-      default = vendor.default_model ? vendor.default_model.config : {}
-      default.deep_merge(values[:config])
-    else
-      values[:config] || {}
-    end
-  end
+
 end
